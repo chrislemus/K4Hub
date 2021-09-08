3a92c529b31aaf667e1e1b60f0528945c3933ff0
chrislemus <icrislemus@gmail.com>
2021-09-08 14:08:38 -0400

    add NavPanel component

--- k4hub/src/App.js
+++ k4hub/src/App.js
@@ -1,10 +1,12 @@
 import NewsList from './components/NewsList';
 import Header from './components/Header';
+import NavPanel from './components/NavPanel';
 function App() {
   return (
     <div>
       <Header />
-      <div>
+      <div className="content">
+        <NavPanel />
         <NewsList />
       </div>
     </div>
---
+++ k4hub/src/components/NavPanel.js
@@ -0,0 +1,8 @@
+export default function NavPanel() {
+  return (
+    <nav className="nav-panel">
+      <button className="nav-item">News</button>
+      <button className="nav-item">Notes</button>
+    </nav>
+  );
+}
